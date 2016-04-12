9f986d37027791ecd13a676d59e53b9a62406793
kingflurkel <mikethuy@gmail.com>
2016-04-12 15:53:13 +0200

    ..

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -34,8 +34,9 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
   app.requestsync = function(){
-    app.$.whisper.whisperpost(app.incomingsecret, JSON.stringify({ 'command': 'sync', 'sender': app.secretcode }));
+    app.$.whisper.whisperpost(app.incomingsecret, JSON.stringify({ 'command': 'sync', 'data': app.secretcode }));
   };
+
   // Scroll page to top and expand header
   app.scrollPageToTop = function() {
     document.getElementById('app').scrollTop = 0;
@@ -67,8 +68,20 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
   };
 
   app.msgreceived = function(e){
-    console.log(e.detail.topic, JSON.parse(e.detail.payload));
+    console.log(e.detail.input.command);
+    var command = e.detail.input.command;
+    var data = e.detail.input.data;
+
+    switch(command){
+      case 'sync':
+      app.sync(data);
+      break;
+    }
+
   };
+
+  // functions activated by device-to-device communication
+  
 
 
 
