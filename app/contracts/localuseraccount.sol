1e500f315146d069b84782665213ab9deeb005be
kingflurkel <mikethuy@gmail.com>
2016-05-18 23:03:37 +0200

    ..

--- app/contracts/localuseraccount.sol
+++ app/contracts/localuseraccount.sol
@@ -3,7 +3,7 @@ contract localsuser {
 	struct User {
 		string username;
 		string ipfshash;
-		mapping (address => mapping (address => string)) verifications;
+		address[] verifications;
 	}
 
 	mapping (address => User) public users;
@@ -16,12 +16,17 @@ contract localsuser {
 		// Save the user's ipfs hashed keystore
 		users[msg.sender].username = _username;
 		users[msg.sender].ipfshash = _ipfshash;
+		users[msg.sender].verifications.push(0x000);
 	}
 
 	function verify(address _localsuser){		
 		// Add a verfier to this user's hash
-		users[_localsuser].verifications[msg.sender];
+		users[_localsuser].verifications.push(msg.sender);
 		// And transfer x localcoin from verifier to user.
+		// Transfer(msg.sender, _localuser, 1);
 	}
 
+    function checkverification(address _localsuser) returns (uint) {
+        return users[_localsuser].verifications.length;
+    }
 }
