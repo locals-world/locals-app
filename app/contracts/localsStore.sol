af700d016bcaa07564c237d16b6493f60c918965
Michael Thuy <mikethuy@gmail.com>
2016-09-16 11:55:37 +0200

    ...

--- app/contracts/localsStore.sol
+++ app/contracts/localsStore.sol
@@ -1,6 +1,9 @@
 import "./owned.sol";
 import "./localsCointoken.sol";
 
+contract token { mapping (address => uint256) public balanceOf;  }
+
+
 contract localsStore is owned {
 
 
@@ -25,127 +28,36 @@ contract localsStore is owned {
     foundation = _foundationContract;
 	}
 
-	function createClub(string _nickname, string _clubname, string _clubicon, address _token)
-		returns (address clubAddress)
-
-	{
-			// Create a new Token contract and return its address.
-			// From the JavaScript side, the return type is simply
-			// "address", as this is the closest type available in
-			// the ABI.
-      // the creator should pay localcoin to the localsfoundation
-      // create an instance of the token contract
+	function createAssociation(uint _minimumQuorum, uint _debatingPeriodInMinutes, token _sharesTokenAddress) returns (address associationAddress)	{
       var tokencontract = localsCointoken(tokenaddr);
 
       if(tokencontract.allowance(msg.sender,this)<200) {
         Error('LocalCoin allowance too low');
         throw;
-        }
+      }
 
       Error('allowance check');
-      /*Allowance('TEST ', token.allowance(msg.sender, this));*/
+      Allowance('TEST ', token.allowance(msg.sender, this));*/
 
       tokencontract.transferFrom(msg.sender, foundation, 200);
 
       Error('localcoin transferred');
-      //token.transfer(foundation, 200);
+      token.transfer(foundation, 200);
 
       // TODO : fix this line
-      //clubAddress = new localsClub(msg.sender, _nickname, _clubicon, _clubname, _token);
+      associationAddress = new Association(_sharesTokenAddress, _minimumQuorum, _debatingPeriodInMinutes);
 
-      ClubCreated(_clubname, clubAddress, msg.sender);
+      ClubCreated(_clubname, associationAddress, msg.sender);
 
-      return clubAddress;
+      return associationAddress;
 
 	}
-
-  function createAssociation(uint _minimumQuorum, uint _debatingPeriodInMinutes, token _sharesTokenAddress) returns (address associationAddress) {
-
-    var tokencontract = localsCointoken(tokenaddr);
-
-    if(tokencontract.allowance(msg.sender,this)<200) {
-      Error('LocalCoin allowance too low');
-      throw;
-      }
-
-    Error('allowance check');
-    /*Allowance('TEST ', token.allowance(msg.sender, this));*/
-
-    tokencontract.transferFrom(msg.sender, foundation, 200);
-
-    Error('localcoin transferred');
-    //token.transfer(foundation, 200);
-
-      // TODO : fix this line
-//    associationAddress = new Association(_minimumQuorum, _debatingPeriodInMinutes, _sharesTokenAddress);
-
-    ClubCreated('association', associationAddress, msg.sender);
-
-    return associationAddress;
-
-  }
 
   /* Kill function, for debug purposes (I don't want a mist wallet full of token contracts :) */
   function kill() { if (msg.sender == owner) suicide(owner); }
 
 }
 
-// Here we start the item contracts
-contract localsClub {
-
-	address public creator;
-  string public clubname;
-  string public clubicon;
-  string public token;
-
-  uint public numMembers;
-  address[] public members;
-
-	struct clubMember {
-		string nickName;
-		bool active;
-	}
-
-  event MemberAdded(string _clubname, string _nickName, address _newmember);
-  event MemberDissed(string _clubname, address _newmember);
-
-	mapping (address => clubMember) public clubMembers;
-
-	function localsClub(address _creator, string _nickName, string _clubicon, string _clubname){
-		creator = _creator;
-    clubname = _clubname;
-    clubicon = _clubicon;
-		clubMembers[_creator].nickName = _nickName;
-		clubMembers[_creator].active = true;
-	}
-
-	// Add a member to the club and make em active
-	function addMember(address _newmember, string _nickName) {
-    //if(msg.sender!=creator) throw;
-    if(clubMembers[_newmember].active) throw;
-		clubMembers[_newmember].nickName = _nickName;
-		clubMembers[_newmember].active = true;
-    members.push(_newmember);
-    numMembers = members.length;
-    MemberAdded(clubname, _nickName, _newmember);
-	}
-
-	// Set a member to active = false
-	function disMember(address _newmember){
-    if(msg.sender!=creator) throw;
-		clubMembers[_newmember].active = false;
-    MemberDissed(clubname, _newmember);
-	}
-
-  // When someone just sends value to the contract
-  function () {
-    throw;
-  }
-
-}
-
-
-contract token { mapping (address => uint256) public balanceOf;  }
 
 // Here we start an Association
 /* The democracy contract itself */
@@ -269,7 +181,13 @@ contract Association is owned {
 
         for (uint i = 0; i <  p.votes.length; ++i) {
             Vote v = p.votes[i];
-            uint voteWeight = sharesTokenAddress.balanceOf(v.voter);
+
+            uint voteWeight = 1;
+
+            if(sharesTokenAddress != 0x0){
+              voteWeight = sharesTokenAddress.balanceOf(v.voter);
+            };
+
             quorum += voteWeight;
             if (v.inSupport) {
                 yea += voteWeight;
